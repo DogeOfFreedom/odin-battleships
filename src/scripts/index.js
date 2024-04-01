671cd46ab85c4c61abb73c4001e1660b8df541a5
DogeOfFreedom <jchensi2000@gmail.com>
2024-04-01 15:33:46 +1100

    feat: created ship class and associated test

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,6 @@
 import "../style.css";
+import Ship from './ship';
 
+
+const myShip = new Ship(3);
+myShip.hit();
--- src/scripts/ship.js
+++ src/scripts/ship.js
@@ -1,18 +1,19 @@
 export default class Ship {
     constructor(length) {
         this.length = length;
-        this.hit = 0;
         this.hp = length;
         this.sunk = false;
     }
 
     hit() {
-        this.hit += 1;
+        this.hp -= 1;
         return this.isSunk();
     }
 
     isSunk() {
-        return this.hp === this.hit
+        return this.hp <= 0;
     }
 }
+
+
 
