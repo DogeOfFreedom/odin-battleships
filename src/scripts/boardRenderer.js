c25cc6a458b24dc904e1a057e2bdc131332dabd8
DogeOfFreedom <jchensi2000@gmail.com>
2024-04-02 14:14:07 +1100

    fix: moved board render logic to seperate function

--- src/scripts/boardRenderer.js
+++ src/scripts/boardRenderer.js
@@ -1,35 +1,27 @@
+const initialiseBoard = (element, board) => {
+    board.forEach(arr => {
+        const newRow = document.createElement("div");
+        newRow.className = "grid-row";
+        arr.forEach(tile => {            
+            const newTile = document.createElement("div");
+            newTile.classList.add("tile");
+            if(tile === "s") { 
+                newTile.classList.add("ship-tile");
+            } 
+            newRow.appendChild(newTile);
+        })
+        element.appendChild(newRow);
+    })
+}
+
 const renderBoards = (game) => {
     const player1BoardElement = document.querySelector(".player1-board");
     const player1Board = game.player1.gameBoard.board;
-    player1Board.forEach(arr => {
-        const newRow = document.createElement("div");
-        newRow.className = "grid-row";
-        arr.forEach(tile => {            
-            const newTile = document.createElement("div");
-            newTile.classList.add("tile");
-            if(tile === "s") { 
-                newTile.classList.add("ship-tile");
-            } 
-            newRow.appendChild(newTile);
-        })
-        player1BoardElement.appendChild(newRow);
-    })
+    initialiseBoard(player1BoardElement, player1Board);
 
     const player2BoardElement = document.querySelector(".player2-board");
     const player2Board = game.player2.gameBoard.board;
-    player2Board.forEach(arr => {
-        const newRow = document.createElement("div");
-        newRow.className = "grid-row";
-        arr.forEach(tile => {            
-            const newTile = document.createElement("div");
-            newTile.classList.add("tile");
-            if(tile === "s") { 
-                newTile.classList.add("ship-tile");
-            } 
-            newRow.appendChild(newTile);
-        })
-        player2BoardElement.appendChild(newRow);
-    })
+    initialiseBoard(player2BoardElement, player2Board);
 }
 
 export { renderBoards }
